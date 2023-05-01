2f85ad982bb3d81205e347346e4a6317dbd2106e
Carter Gaulke <cgaulke3@gmail.com>
2023-04-30 20:32:58 -0400

    Inital creation of chess_bot.py

---
+++ chess_bot.py
@@ -0,0 +1,15 @@
+'''
+This file is used to train our machine learning model
+for our chess game
+
+It will allow the model to make a move and then the user
+to input what the reqrd for the move was
+'''
+
+## input the chess playing ability
+
+# add a function that will find all the available moves for all pieces of a color
+
+# set up and train the model
+
+# add functinality to input what the reward of a move was
